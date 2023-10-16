9137b956bcd4397f341243ae275169b04e82b6b0
FouHamster <alina63111@gmail.com>
2023-10-16 14:15:57 +0700

    and4

--- django_project/locallibrary/catalog/models.py
+++ django_project/locallibrary/catalog/models.py
@@ -34,6 +34,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
@@ -83,3 +89,5 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return '{0}, {1}'.format(self.last_name, self.first_name)
+
+
