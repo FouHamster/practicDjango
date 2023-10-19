b515154e8548849c0744ff1592908954e1a51637
unknown <locadm@O401-10.ttit.local>
2023-10-19 12:32:59 +0700

    and9

--- django_project/locallibrary/catalog/urls.py
+++ django_project/locallibrary/catalog/urls.py
@@ -15,3 +15,13 @@ urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
+
