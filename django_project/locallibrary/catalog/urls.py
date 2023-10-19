c1322347bff6b7f9c4b0356ac7137f9f4702af67
unknown <locadm@O401-10.ttit.local>
2023-10-19 11:30:16 +0700

    end7

--- django_project/locallibrary/catalog/urls.py
+++ django_project/locallibrary/catalog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- django_project/locallibrary/catalog/views.py
+++ django_project/locallibrary/catalog/views.py
@@ -15,12 +15,15 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -31,4 +34,9 @@ from django.views import generic
 class BookListView(generic.ListView):
     model = Book
 
+class BookDetailView(generic.DetailView):
+    model = Book
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
