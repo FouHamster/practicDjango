78716f2812ed1f405b0f95a98a39a1c6d01a20fb
FouHamster <alina63111@gmail.com>
2023-10-25 11:42:10 +0700

    and10

--- django_project/locallibrary/catalog/models.py
+++ django_project/locallibrary/catalog/models.py
@@ -101,4 +101,12 @@ class Author(models.Model):
         """String for representing the Model object."""
         return '{0}, {1}'.format(self.last_name, self.first_name)
 
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
 
